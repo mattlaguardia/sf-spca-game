aa24f764d068c69b72eebe96f80af8b4aec5362a
mattlaguardia <matthewlaguardia@gmail.com>
2016-11-14 16:28:43 -0800

    adds some basic functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,8 @@
 console.log("sanity check - js")
+
+var game = new Phaser.Game(320, 480, Phaser.CANVAS, 'game', { preload: preload })
+
+function preload() {
+  game.stage.backgroundColor = '#333'
+  game.state.add('Boot', Ball.Boot)
+}
